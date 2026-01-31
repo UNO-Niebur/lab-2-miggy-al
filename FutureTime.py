17a6d09db56d9cb3ae50e2c8c84d26f2889d7cb9
miggy-al <03766542@nebraska.edu>
2026-01-31 01:20:14 +0000

    Finished with Lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Miguel Alvarado
+#Date: 1/29/2026
+#Assignment: Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,21 +9,29 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  print("Ask me for hours")
+  Hours = int(input("hours:"))
+   
   #Ask user for minutes
-
+  print("Ask me for minutes")
+  addMinute= int(input("minutes:"))
   #Calculate the time after the user-supplied time has passed.
-
+  futuretime = (currentMinute + addMinute) 
+  newMinutes = (futuretime) % 60
+  extraHours = (futuretime) // 60
+  
+  futureHour = (currentHour + Hours + extraHours) % 24
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+  print("the future time is", str(futureHour).zfill(2) + ":" + str(newMinutes).zfill(2))
 
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,18 +1,26 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Miguel Alvarado
+#Date: 1/29/2026
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+ print("Magic 8 Ball")
   #Prompt the user for their question.
+ print("Hello" +"!")
+ print("Ask me any question and I'll decide your fate")
+ input("")
+ 
+ answers = ["nah", "are we serious right now bro?", "As I see it, yes", "You may rely on it","maybe",
+             "mmmm I'll think about it later","Very doubtful", "Yesss bro", "Noooooooo", 
+             "don't get your hopes up bud", "Most likely", "Most likely not"]
 
   #Answer question randomly with one of the options from your earlier list.
-
+ response = random.choice(answers)
+ print(response)
 
 if __name__ == '__main__':
   main()
